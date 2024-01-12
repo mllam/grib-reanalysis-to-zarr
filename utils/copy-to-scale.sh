6ea9f1c8375b899350baf3b80c7c3efc8ce7a312
Leif Denby <lcd@dmi.dk>
2024-01-12 13:05:18 +0000

    add utility for copy to scale

---
+++ utils/copy-to-scale.sh
@@ -0,0 +1,14 @@
+DEST_DIR=/dmidata/projects/cloudphysics/danra/data
+
+# get version number to copy from command line
+if [ $# -eq 0 ]
+  then
+    echo "usage: copy-to-scale.sh <version>"
+    exit 1
+else
+    VERSION=$1
+    echo "Copying version $VERSION to scale.dmi.dk"
+fi
+
+rsync --progress -r "${VERSION}" "${DEST_DIR}/${VERSION}"
+cp ~/git-repos/danra_to_zarr/CHANGELOG.md "${DEST_DIR}/"
