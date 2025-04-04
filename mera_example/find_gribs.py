523c97760f5eaacdc4caef30b00efa4ff50c7570
Kasper Hintz <kah@dmi.dk>
2025-04-04 09:02:35 +0000

    correct find_gribs

--- mera_example/find_gribs.py
+++ mera_example/find_gribs.py
@@ -6,9 +6,11 @@ def find_files_for_time_interval(t_start: datetime, t_end: datetime) -> list[str
     root_path = Path("/scratch/duuw/reanalysis/mera/")
 
     # Example: MERA_PRODYEAR_2017_12_11_105_2_0_ANALYSIS
-    filename_format = "MERA_PRODYEAR_{start:%Y_%m_%d%H}_{leveltype}_{level}_{timerangeindicator}_{suffix}"
+    path_format = "{indicatorofparameter}/{leveltype}/{level}/{timerangeindicator}"
+    filename_format = "MERA_PRODYEAR_{start:%Y_%m}_{indicatorofparameter}_{leveltype}_{level}_{timerangeindicator}_{suffix}"
     suffixes = ["ANALYSIS"]
     
+    indicatorofparameter = "11"
     leveltype = "105"
     level = "2"
     timerangeindicator = "0"
@@ -16,12 +18,20 @@ def find_files_for_time_interval(t_start: datetime, t_end: datetime) -> list[str
     paths = []
     t = t_start
     for suffix in suffixes:
-        for hours in range(4):
-            filename = filename_format.format(start=t, 
-                                              leveltype=leveltype,
-                                              level=level,
-                                              timerangeindicator=timerangeindicator,
-                                              suffix=suffix)
-            paths.append(root_path / filename)
 
+        path_file = path_format.format(indicatorofparameter=indicatorofparameter,
+                                        leveltype=leveltype,
+                                        level=level,
+                                        timerangeindicator=timerangeindicator)
+
+        filename = filename_format.format(start=t,
+                                          indicatorofparameter=indicatorofparameter,
+                                          leveltype=leveltype,
+                                          level=level,
+                                          timerangeindicator=timerangeindicator,
+                                          suffix=suffix)
+
+        paths.append(root_path / path_file / filename)
+
+    print(paths)
     return paths
